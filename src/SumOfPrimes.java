1673a780a86705f92f469460f07a96978d1791f2
Binley Yang <byang8@u.rochester.edu>
2012-11-07 15:05:33 -0500

    updates to sumofprimes

--- src/SumOfPrimes.java
+++ src/SumOfPrimes.java
@@ -3,14 +3,14 @@ public class SumOfPrimes {
 	
 	public static void main (String[] args) {
 		
-		int sum = 0;
-		for (int i = 2; i <= 2000000; i++) {
+		long sum = 0;
+		for (int i = 2; i < 2000000; i++) {
 			if (primecheck(i) == true) {
 				sum += i;
-				System.out.println("The sum of all primes below two million is " + sum + ".");
+				System.out.println(i + " : " + sum); //tracking the progress of the program
 			}
 		}
-
+		System.out.println("The sum of all primes below two million is " + sum + "."); //display the answer
 		
 	}
 
