816ab507fa06144c517cf51cdc8224febd34c880
Binley <byang8@u.rochester.edu>
2012-11-15 20:28:07 -0500

    minor fixes

--- src/Problem9.java
+++ src/Problem9.java
@@ -4,8 +4,8 @@ public class Problem9 {
 	public static void main (String[] args) {
 		
 		int product = 1;
-		for (int i = 0; i < 1000; i++) {
-			for (int j = 0; j < 1000; j++) {
+		for (int i = 1; i < 1000; i++) {
+			for (int j = 1; j < 1000; j++) {
 				if(Math.pow((i*i + j*j), 0.5) - Math.ceil(Math.pow((i*i + j*j), 0.5)) == 0 && i + j + Math.pow(i*i + j*j, 0.5) == 1000) {
 					product = (int) (i*j*(Math.pow(i*i + j*j, 0.5)));
 					System.out.println("The product of abc when a + b + c = 1000 is " + product);
