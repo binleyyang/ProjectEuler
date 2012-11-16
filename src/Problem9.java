fddd8841c794d97420f36effdc9184d53409033e
Binley <byang8@u.rochester.edu>
2012-11-15 20:23:29 -0500

    minor update

--- src/Problem9.java
+++ src/Problem9.java
@@ -6,7 +6,6 @@ public class Problem9 {
 		int sum = 0, product = 1;
 		for (int i = 0; i < 1000; i++) {
 			for (int j = 0; j < 1000; j++) {
-				//int csquared = i*i + j*j;
 				if(Math.pow((i*i + j*j), 0.5) - Math.ceil(Math.pow((i*i + j*j), 0.5)) == 0 && i + j + Math.pow(i*i + j*j, 0.5) == 1000) {
 					product = (int) (i*j*(Math.pow(i*i + j*j, 0.5)));
 					System.out.println("The product of abc when a + b + c = 1000 is " + product);
