4ff9c3c379c0f372336b55a739b798ce45102c2c
binleyyang <ybinley@gmail.com>
2012-12-11 21:28:23 -0500

    small update

--- src/Problem7.java
+++ src/Problem7.java
@@ -4,7 +4,7 @@ public class Problem7 {
 	public static void main (String[] args) {
 		int counter = 0;
 		for (int i = 2; i <= 10000000; i++) {
-			if (primecheck(i) == true){
+			if (primeChecker(i) == true){
 				counter++;
 			}
 				if (counter == 10001) {
@@ -15,10 +15,10 @@ public class Problem7 {
 		}
 	}
 	
-	public static boolean primecheck(int x){
+	public static boolean primeChecker(int x){
 
-		for (int counter = 2; counter < x; counter++){
-			if (x % counter == 0)
+		for (int count = 2; count < x; count++){
+			if (x % count == 0)
 				return false;
 		}
 		return true;
